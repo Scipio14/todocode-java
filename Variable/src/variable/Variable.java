b946c5225199db929e5c77e9b81d7fd4ef962055
Federico Macías <fma.abogado@gmail.com>
2021-09-14 09:34:46 -0500

    Condicionales anidadas

--- Variable/src/variable/Variable.java
+++ Variable/src/variable/Variable.java
@@ -6,12 +6,16 @@ public class Variable {
   
     public static void main(String[] args) {
         int num1 = 25;
-        int num2 = 10;
+        int num2 = 25;
         
         if(num2 > num1){
             System.out.println("El num2 es mayor a num1");
         }else{
-            System.out.println("El num1 es mayor al num2");
+            if(num1 == num2){
+                System.out.println("Son iguales");
+            }else{
+                System.out.println("El num1 es mayor al num2");
+            }
         }
     }
     
