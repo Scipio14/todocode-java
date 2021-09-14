274ef47c2e2c9e86aa16e48d1c8d77affb949bb8
Federico Macías <fma.abogado@gmail.com>
2021-09-14 09:21:32 -0500

    Sentencia if-else

---
+++ Variable/src/variable/Variable.java
@@ -0,0 +1,18 @@
+
+package variable;
+
+public class Variable {
+
+  
+    public static void main(String[] args) {
+        int num1 = 25;
+        int num2 = 10;
+        
+        if(num2 > num1){
+            System.out.println("El num2 es mayor a num1");
+        }else{
+            System.out.println("El num1 es mayor al num2");
+        }
+    }
+    
+}
