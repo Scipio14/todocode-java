176f5d6b2bf2baeae408d25590f34ce39b21e6ce
Federico Macías <fma.abogado@gmail.com>
2021-09-15 08:32:26 -0500

    Estructuras repetitivas

---
+++ repetitivas/src/repetitivas/Repetitivas.java
@@ -0,0 +1,24 @@
+
+package repetitivas;
+
+public class Repetitivas {
+
+    public static void main(String[] args) {
+      //ejemplo while
+        /*int cont =0;
+        while(cont <= 10){
+          System.out.println("Estoy en la vuelta " + cont);
+          cont = cont +1;
+          
+      
+      
+      }
+        */
+        //ejemplo for
+        
+        for (int cont=0; cont <=10; cont++ ){
+        System.out.println("Estoy dando vueltas con for, estoy en la vuelta: " + cont);
+        }
+    }
+    
+}
