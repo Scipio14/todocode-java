026b2538e8d81499923d846cab6e4ccfc05f8ac8
Federico Macías <fma.abogado@gmail.com>
2021-09-16 10:00:22 -0500

    Matrices

---
+++ Matrices/src/matrices/Matrices.java
@@ -0,0 +1,37 @@
+
+package matrices;
+
+import java.util.Scanner;
+
+public class Matrices {
+
+   
+    public static void main(String[] args) {
+    //Declaracion
+    
+    int matriz [][] = new int [3][3];
+    
+    Scanner teclado = new Scanner (System.in);
+    
+    //Asignacion teclado
+    
+   for(int f=0;f<3; f++){
+        for(int c=0; c<3; c++){
+        System.out.println("Ingrese el valor de la posición f: " + f + " c: " +c);
+        matriz[f][c] = teclado.nextInt();
+        
+        }
+    }
+    
+    //Recorrido
+    
+    for(int f=0;f<3; f++){
+        for(int c=0; c<3; c++){
+        System.out.println("El valor de la posición f: " + f +" c: " +c);
+        System.out.println("es de: " + matriz[f][c]);
+        
+        }
+    }
+    }
+    
+}
