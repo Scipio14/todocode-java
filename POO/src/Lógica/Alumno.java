cfd5a5d105119aeecc7368bb8d1a0c1befddb921
Federico Macías <fma.abogado@gmail.com>
2021-10-21 12:16:14 -0500

    Viendo los métodos

--- POO/src/Lógica/Alumno.java
+++ POO/src/Lógica/Alumno.java
@@ -7,6 +7,18 @@ public class Alumno {
     String nombre;
     String apellido;
     
+    public void mostrarNombre(){
+        System.out.println("Hola, soy un alumno y sé decir mi nombre");
+    }
     
+    public void saberAprobado(double calificacion){
+        if (calificacion >=6){
+            System.out.println("Aprobé la materia");
+        
+        }else{
+            System.out.println("Uyy no aprobé la materia");
+        }
+    
+    }
     
 }
