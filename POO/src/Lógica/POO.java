c35fd4b9090b291529a549bd9570e61dba6c1c81
Federico Macías <fma.abogado@gmail.com>
2021-11-02 21:36:36 -0600

    Viendo getters y setters

--- POO/src/Lógica/POO.java
+++ POO/src/Lógica/POO.java
@@ -7,6 +7,24 @@ public class POO {
    
     public static void main(String[] args) {
         
+        Alumno alu1 = new Alumno();
+        Alumno alu2 = new Alumno(5,"Sicpio","Scipio");
+        
+        System.out.println("La id del alumno 2 es: " + alu2.getId());
+        System.out.println("El nombre es:  " + alu2.getNombre());
+        System.out.println("-----------------------------------");
+        alu1.setId(8);
+        alu1.setNombre("Suscribete");
+        alu2.setApellido("Todocode");
+        System.out.println("La id del alumno 1 es: " + alu1.getId());
+        System.out.println("El nombre es:  " + alu1.getNombre());
+        System.out.println("-----------------------------------");
+        //cambio de valor
+        alu2.setId(35);
+        System.out.println("-----------------------------------");
+        System.out.println("La id del alumno 2 es: " + alu2.getId());
+        System.out.println("El nombre es:  " + alu2.getNombre());
+            
         
         
         
